dee765e4c417f36c8b8c8a17dcbd8bec4b0b81c7
Akash Limbani <akashlimbani22@gmail.com>
2022-12-23 11:44:05 +0530

    food ecommerce store added

---
+++ components/footer.js
@@ -0,0 +1,15 @@
+export const footer = () => {
+    return (
+        `<div id="footer">
+            <p class="text-center">Made with ❤️ by Aakash Limbani</p>
+            <div>
+                <button class="btn btn-primary">
+                    <a href="https://www.linkedin.com/in/aakash-limbani/" target="_blank">LinkedIn</a>
+                </button>
+                <button class="btn btn-success">
+                    <a href="https://github.com/aakash-limbani" target="_blank">GitHub</a>
+                </button>
+            </div>
+        </div>`
+    );
+};
